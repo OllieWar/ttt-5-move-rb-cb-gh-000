648fdf283afa516c75da892ccbd3f7b2fa9dcbf9
Oliver Warway <olliewarway@gmail.com>
2017-11-21 15:46:41 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(input_num)
   input_num.to_i
 end
 
-def move(board, input_num, players_char)
-  value = board[input_num] 
+def move(array, index, value)
+  value = board[input_num]
   display_board(value)
 end
